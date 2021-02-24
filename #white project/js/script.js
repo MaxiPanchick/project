be1fb7ae5639ef142f7dea13a50dad7cea8fc9a4
maxipan <eaxample@gmail.com>
2021-02-24 13:49:38 +0300

    first commit

---
+++ #white project/js/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+const answers = [];
+
+answers[0] = prompt('What is Your name?');
+answers[1] = prompt('What is Your soname?');
+answers[2] = prompt('What is Your opinion?');
+
+document.write(answers);

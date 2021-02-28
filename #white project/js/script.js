80e8239e363c8b9ea629c262ee42e36145e4f32d
maxipan <eaxample@gmail.com>
2021-02-28 13:57:38 +0300

    цикл for

--- #white project/js/script.js
+++ #white project/js/script.js
@@ -1,18 +1,8 @@
 "use strict";
 
-const number0fFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
-const personalMovieDB = {
-    count: number0fFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB);
+for (let i = 1; i < 10; i++) {
+    if (i === 7) {
+        continue;
+    }
+    console.log(i);
+}

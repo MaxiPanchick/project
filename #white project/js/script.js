c00f2a5c93d8873ec7e8402d8e9b17eac53b6dbc
maxipan <eaxample@gmail.com>
2021-02-28 12:14:23 +0300

    pract1

--- #white project/js/script.js
+++ #white project/js/script.js
@@ -1,10 +1,18 @@
 "use strict";
 
+const number0fFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-const answers = [];
-
-answers[0] = prompt('What is Your name?');
-answers[1] = prompt('What is Your soname?');
-answers[2] = prompt('What is Your opinion?');
-
-document.write(answers);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
